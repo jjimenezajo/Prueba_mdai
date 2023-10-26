3f2c28889c8401edcc8d3c21f3862c02d83adf80
AlvaroBF01 <alvarobrfz@gmail.com>
2023-10-26 18:52:17 +0200

    Update Carpeta.java

--- Studient/src/main/java/es/unex/cum/mdai/studient/model/Carpeta.java
+++ Studient/src/main/java/es/unex/cum/mdai/studient/model/Carpeta.java
@@ -1,5 +1,18 @@
 package es.unex.cum.mdai.studient.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Carpeta {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String nombre;
+	private boolean mutabilidad;
+	
+	
+	
 
 }
