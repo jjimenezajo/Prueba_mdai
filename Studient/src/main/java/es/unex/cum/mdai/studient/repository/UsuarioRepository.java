7b5daf3c26fddff1aaf616709e6bcb609b05e6b9
AlvaroBF01 <alvarobrfz@gmail.com>
2023-10-31 17:25:32 +0100

    Tests 1.0

--- Studient/src/main/java/es/unex/cum/mdai/studient/repository/UsuarioRepository.java
+++ Studient/src/main/java/es/unex/cum/mdai/studient/repository/UsuarioRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import es.unex.cum.mdai.studient.model.Usuario;
 
 
- interface UsuarioRepository extends CrudRepository<Usuario, Long>{
+public interface UsuarioRepository extends CrudRepository<Usuario, Long>{
 
 }
--- Studient/src/test/java/es/unex/cum/mdai/studient/StudientApplicationTests.java
+++ Studient/src/test/java/es/unex/cum/mdai/studient/StudientApplicationTests.java
@@ -1,13 +1,30 @@
 package es.unex.cum.mdai.studient;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import es.unex.cum.mdai.studient.repository.*;
 
 @SpringBootTest
 class StudientApplicationTests {
 
+	@Autowired
+	UsuarioRepository usuarioRepository;
+	
+	@Autowired
+	CarpetaRepository carpetaRepository;
+	
+	@Autowired
+	TareaRepository tareaRepository;
+	
 	@Test
 	void contextLoads() {
+	
+		
+		
+		
+		
+		
 	}
 
 }
